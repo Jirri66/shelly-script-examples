dbfcfbd90ad6fd1e99679d9e7f169c72df55f216
Jirri66 <jirri@web.de>
2024-10-19 05:31:51 +0200

    Update control_rgbw.js
    First Edit the Push Button to the actions, then add color white from the rgbw2

--- control_rgbw.js
+++ control_rgbw.js
@@ -11,27 +11,28 @@
 // CONFIG START
 // IP address or hostname from RGBW device
 let REMOTE = {
-    ip: '192.168.178.71',
+    ip: '192.168.178.92',
+    input: 3, // ID from the push button: 0 for Shelly Plus 1 / Plus 1 PM or 0,1,2 or 3 for the Shelly I4.
 };
 
 // button sequence 
-// The demo sequence is: on and blue with max brightness, then red, green, yellow, purple, off
+// The demo sequence is: on white with 50% brightness, then blue, red, green, yellow, purple
 let colorsequence = [
-   '?turn=on&gain=100&blue=0&red=255&green=120', // warm white
-   '?turn=on&gain=100&blue=255&red=0&green=0', // blue
-   '?turn=on&gain=100&blue=0&red=255&green=0', // red
-   '?turn=on&gain=100&blue=0&red=0&green=255', // green
-   '?turn=on&gain=100&blue=0&red=255&green=255', // yellow
-   '?turn=on&gain=100&blue=225&red=185&green=76', // purple
+   '?turn=on&gain=50&blue=0&red=0&green=0&white=255', // warm white
+   '?turn=on&gain=50&blue=255&red=0&green=0&white=0', // blue
+   '?turn=on&gain=50&blue=0&red=255&green=0&white=0', // red
+   '?turn=on&gain=50&blue=0&red=0&green=255&white=0', // green
+   '?turn=on&gain=50&blue=0&red=255&green=255&white=0', // yellow
+   '?turn=on&gain=50&blue=225&red=185&green=76&white=0', // purple
 ];
 
 // Dimming level for the related color
 let dimsequence = [
-   '?gain=50', // 50%
    '?gain=33', // 33%
    '?gain=20', // 20%
    '?gain=5', // 5%
    '?gain=100', // 100%
+   '?gain=50', // 50%
 ];
 // CONFIG END 
 
@@ -45,7 +46,7 @@ Shelly.addEventHandler(
         //print(JSON.stringify(event));
         if (typeof event.info.event !== 'undefined') {
            
-            if (event.info.event === 'double_push') {
+            if (event.info.event === 'double_push'&& event.info.id === REMOTE.input) {
                 setRGBW(REMOTE.ip, colorposition);
                 
                 colorposition++;
@@ -54,10 +55,10 @@ Shelly.addEventHandler(
                   colorposition = 0;
                 }
 
-           } else if (event.info.event === 'single_push'){
+           } else if (event.info.event === 'single_push'&& event.info.id === REMOTE.input){
                toggleRGBW(REMOTE.ip);
 
-           } else if (event.info.event === 'long_push') {
+           } else if (event.info.event === 'long_push'&& event.info.id === REMOTE.input) {
                dimRGBW(REMOTE.ip, dimpos);
                dimpos++;
                if (dimsequence.length === dimpos) {
